96a261796ff68ba9095f24b5aedb82d083ed08ae
Angela Zhang <angelafzhang@gmail.com>
2013-07-24 14:08:25 -0700

    Initial Commit

---
+++ Ubiquity/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ubiquity
+//
+//  Created by Angela Zhang on 7/24/13.
+//  Copyright (c) 2013 Team Ubi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UbiquityTests/UbiquityTests.m
@@ -0,0 +1,36 @@
+//
+//  UbiquityTests.m
+//  UbiquityTests
+//
+//  Created by Angela Zhang on 7/24/13.
+//  Copyright (c) 2013 Team Ubi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UbiquityTests : XCTestCase
+
+@end
+
+@implementation UbiquityTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
