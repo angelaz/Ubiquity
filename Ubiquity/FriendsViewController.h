6648d6bc52f8fd50e9d52c7d56484e139b52fc30
Ada Taylor <ada@idu.com>
2013-08-06 13:16:58 -0700

    FriendsViewHeader has no more UbiquityFriends reference

--- Ubiquity/FriendsViewController.h
+++ Ubiquity/FriendsViewController.h
@@ -17,8 +17,6 @@
 
     CustomFBFriendPickerViewController *friendPickerController;
     NSMutableArray* selectedFriends;
-
-    PFObject *ubiquityFriends;
     
     //View objects
     __weak IBOutlet UITableView *menuTableView;
